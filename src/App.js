34846c14ea52eeaa57f34a2bd438061b6a4c4589
alotfy2019 <elshenawy19@gmail.com9>
2021-02-25 12:26:12 +0200

    initialize git repo for react course

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: 'asc1'
+        },
+        {
+          name: 'Dracula',
+          id: 'asc2'
+        },
+        {
+          name: 'Zombie',
+          id: 'asc3'
+        }
+      ]
+    }
+  }
+
+  componentDidMount()
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map(monsters => (
+          <h1 key={monsters.id}> {monsters.name}</h1>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
